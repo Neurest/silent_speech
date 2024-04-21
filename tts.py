b4788f553962451efb8a977afd02dd9a9086aa68
nemodleo <nemod.leo@snu.ac.kr>
2024-04-21 14:52:23 +0900

    add auto make dirs

--- tts.py
+++ tts.py
@@ -1,3 +1,4 @@
+import os
 import json
 import argparse
 from glob import glob
@@ -18,9 +19,11 @@ if __name__ == "__main__":
 
     for info_path in tqdm(glob(f"{args.input_folder}/**/*_info.json", recursive=True)):
         info = json.load(open(info_path))
+        out_file_path=info_path.replace(args.input_folder, args.output_folder).replace('_info.json', '_tts.wav')
+        os.makedirs(os.path.dirname(out_file_path), exist_ok=True)
         tts.tts_to_file(
             text=info['text'],
-            file_path=info_path.replace(args.input_folder, args.output_folder).replace('_info.json', '_tts.wav'),
+            file_path=out_file_path,
             speaker_wav=args.speaker_voice,
             language=args.language
         )
